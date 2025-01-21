59cf7c7652973c0f5cb6e29b8654a105f3205aed
Luciano Montilla <unonueve95@gmail.com>
2025-01-21 12:14:17 -0300

    Update font family

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,12 +1,21 @@
 import { Link, Outlet, useLocation } from "react-router";
 import { AuthProvider } from "../components/auth/authProvider";
 
+const Welcome = () => {
+    return (
+        <div className="h-[calc(100vh-32px)] flex flex-col items-center justify-between py-14">
+            <h1>Welcome!</h1>
+            <Link to="signin">Start</Link>
+        </div>
+    );
+};
+
 export const Root = () => {
     const location = useLocation();
 
     return (
-        <div className="bg-red-500 min-w-screen min-h-screen">
-            {location.pathname === "/" && <Link to="signin">Start</Link>}
+        <div className="min-h-screen p-4 min-w-screen font-geist">
+            {location.pathname === "/" && <Welcome />}
             <AuthProvider>
                 <Outlet />
             </AuthProvider>
